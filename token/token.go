749eb5c1c65407054885a0195e92f22d759b30c7
Gregor Zurowski <gregor@zurowski.org>
2026-02-22 00:08:20 +0100

    Add identifier lookup

--- token/token.go
+++ token/token.go
@@ -35,3 +35,15 @@ type Token struct {
 func New(tokenType Type, literal string) Token {
 	return Token{Type: tokenType, Literal: literal}
 }
+
+var keywords = map[string]Type{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+func LookupIdent(ident string) Type {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
