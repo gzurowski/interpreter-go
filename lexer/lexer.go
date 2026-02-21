1310713a941fa875444fe06210150712729af511
Gregor Zurowski <gregor@zurowski.org>
2026-02-22 00:19:00 +0100

    [Minor] Group predicate functions

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -71,6 +71,10 @@ func isWhitespace(ch byte) bool {
 	return ch == ' ' || ch == '\t' || ch == '\n' || ch == '\r'
 }
 
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
+}
+
 func isLetter(ch byte) bool {
 	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
 }
@@ -83,8 +87,4 @@ func (l *Lexer) readChar() {
 	}
 	l.position = l.readPosition
 	l.readPosition++
-}
-
-func isDigit(ch byte) bool {
-	return '0' <= ch && ch <= '9'
 }
