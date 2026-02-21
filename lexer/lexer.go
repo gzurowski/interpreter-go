820b5c481d2ede48203a6339bab04b279aaecacb
Gregor Zurowski <gregor@zurowski.org>
2026-02-21 23:42:12 +0100

    Add basic lexer implementation

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,5 +1,7 @@
 package lexer
 
+import "github.com/gzurowski/monkey/token"
+
 type Lexer struct {
 	input        string
 	position     int  // current position in input (current character)
@@ -9,5 +11,48 @@ type Lexer struct {
 
 func New(input string) *Lexer {
 	lexer := &Lexer{input: input}
+	lexer.readChar()
 	return lexer
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	switch l.ch {
+	case '=':
+		tok = token.New(token.ASSIGN, string(l.ch))
+	case '+':
+		tok = token.New(token.PLUS, string(l.ch))
+	case '(':
+		tok = token.New(token.LPAREN, string(l.ch))
+	case ')':
+		tok = token.New(token.RPAREN, string(l.ch))
+	case '{':
+		tok = token.New(token.LBRACE, string(l.ch))
+	case '}':
+		tok = token.New(token.RBRACE, string(l.ch))
+	case ',':
+		tok = token.New(token.COMMA, string(l.ch))
+	case ';':
+		tok = token.New(token.SEMICOLON, string(l.ch))
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	default:
+		tok.Type = token.ILLEGAL
+		tok.Literal = string(l.ch)
+	}
+
+	l.readChar()
+	return tok
+}
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition++
 }
